f9a0f1260d92be637d6af871e31283ec6cdd860e
Phumuza Sibisi <109278974+sibisiphumuza@users.noreply.github.com>
2023-12-23 23:39:39 +0200

    initial submission

---
+++ Guru99BankingDemo/SmokeTests.cs
@@ -0,0 +1,21 @@
+﻿using BankingTestFramework;
+using BankingTestFramework.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guru99BankingDemo
+{
+    [TestFixture]
+    public class SmokeTests : TestBase
+    {
+        [Test]
+        public void CanLogIn()
+        {
+            Pages.Login.LogIn();
+            Assert.That(Pages.Login.IsLoggedIn(), Is.True);
+        }
+    }
+}
